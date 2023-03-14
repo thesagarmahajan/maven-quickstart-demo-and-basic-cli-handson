e09a695c40e03c62ff5ee27a032da2115fef2d36
thesagarmahajan <sagarmahajan31997@gmail.com>
2023-03-14 14:19:42 +0530

    Quick Project Upload

---
+++ src/main/java/trainings/specialization/demo/App.java
@@ -0,0 +1,13 @@
+package trainings.specialization.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
